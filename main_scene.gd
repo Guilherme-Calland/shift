fd39681b1e41410e0c6b02ca48e96b7f3c45011a
Guilherme <gui.crb0@gmail.com>
2024-02-03 15:46:47 -0300

    2 gears

--- main_scene.gd
+++ main_scene.gd
@@ -1,12 +1,24 @@
 extends Control
 
 var speed : float = 0
-@export var aceleration : float = 0.8
+var gear : int = 1
+
+@export var aceleration : float = 1
 
 func _process(_delta):
 	if(Input.is_action_pressed("acelerate")):
-		speed+=aceleration
+		speed+=(aceleration/gear)
 	else:
 		speed = clamp(speed - aceleration, 0, speed)
 	
+	if speed >= 60:
+		if gear == 1:
+			speed = 60
+	
+	if(Input.is_action_just_pressed("shift up")):
+		gear += 1
+	elif(Input.is_action_just_pressed("shift down")):
+		gear -= 1
+	
 	$Speed.text = str(floor(speed))
+	$Gear.text = str(gear)
