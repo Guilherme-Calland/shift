f3000051df8b4d5c8965549ff2810fc793304007
Guilherme <gui.crb0@gmail.com>
2024-02-03 15:59:46 -0300

    6 clutches

--- main_scene.gd
+++ main_scene.gd
@@ -4,21 +4,22 @@ var speed : float = 0
 var gear : int = 1
 
 @export var aceleration : float = 1
+@export var speedShift : int = 30
 
 func _process(_delta):
 	if(Input.is_action_pressed("acelerate")):
-		speed+=(aceleration/gear)
+		if(speed < speedShift * gear):
+			speed = clamp(speed + (aceleration/gear), 0, speedShift * gear)
 	else:
 		speed = clamp(speed - aceleration, 0, speed)
 	
-	if speed >= 60:
-		if gear == 1:
-			speed = 60
+	if speed >= speedShift * gear:
+		speed = clamp(speed - aceleration, speedShift * gear, speed)
 	
 	if(Input.is_action_just_pressed("shift up")):
-		gear += 1
+		gear = clamp(gear + 1, 0, 6)
 	elif(Input.is_action_just_pressed("shift down")):
-		gear -= 1
+		gear = clamp(gear - 1, 0, 6)
 	
 	$Speed.text = str(floor(speed))
 	$Gear.text = str(gear)
