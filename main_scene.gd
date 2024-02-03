6ef3dc2efc9e47541e2381ab2f29ca766aca71d9
Guilherme <gui.crb0@gmail.com>
2024-02-03 15:29:41 -0300

    acelerating on 1 gear

--- main_scene.gd
+++ main_scene.gd
@@ -1,11 +1,12 @@
 extends Control
 
-var speed = 0
+var speed : float = 0
+@export var aceleration : float = 0.8
 
-func _process(delta):
+func _process(_delta):
 	if(Input.is_action_pressed("acelerate")):
-		speed+=1
+		speed+=aceleration
 	else:
-		speed = clamp(speed - 1, 0, speed)
+		speed = clamp(speed - aceleration, 0, speed)
 	
-	$Speed.text = str(speed)
+	$Speed.text = str(floor(speed))
